cb665be471d8c05424086e4ddba12a51804a0864
Soko10v <43305460+Soko10v@users.noreply.github.com>
2026-06-17 14:45:51 +0300

    lessons of most useless feture of coding(recursion)
    курсор бы за 2 секнуды навайбкодид

--- task3.lesson6.cpps47.vak.cpp
+++ task3.lesson6.cpps47.vak.cpp
@@ -2,18 +2,23 @@
 #include <iostream>
 
 int fib(int n) {
-    if (n == 0) {
-        return 0;
-    }
-    if (n == 1) {
-        return 1;
+    // if (n == 0) {
+    //     return 0;
+    // }
+    // if (n == 1) {
+    //     return 1;
+    // }
+    if (n<1) {
+        return n;
     }
     return fib(n - 1) + fib(n - 2);
 }
 
-int main(int argc, char** argv) {
-    (void)argc;
-    (void)argv;
+// int main(int argc, char** argv) {
+    // (void)argc;
+    // (void)argv;
+    // всем поохуй на твои аргс аргв если не знаешь не пиши 
+    int main (){
     // У меня часто отваливается кодировка, поэтому я задаю локаль
     setlocale(LC_ALL, "ru_RU.UTF-8");
 
